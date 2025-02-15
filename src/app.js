10755d08f9c1c8fa442879740bdc340e03ff53d5
Zaheen335 <akhtarzaheen335@gmail.com>
2025-02-15 17:42:28 +0530

    explored post, get and delete requests

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ app.use('/test',(req,res) => {
     res.send('response for /test')
 })
 
+
+
+app.post("/user",(req,res) => {
+    res.send({firstName:"Zaheen",lastName:"Akhtar"})
+})
+
+app.get('/user',(req,res) => {
+    res.send('fetched user details')
+});
+
+app.delete('/user',(req,res) => {
+    res.send('delete user')
+})
+
 app.use("/",(req,res) => {
     res.send('response for /')
 })
