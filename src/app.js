c69a7488eb14872e503e629215f22d516316d274
Zaheen335 <akhtarzaheen335@gmail.com>
2025-03-04 09:20:23 +0530

    multiple-route-handler

--- src/app.js
+++ src/app.js
@@ -2,28 +2,50 @@ const express = require("express");
 
 const app = express();
 
-app.use('/test',(req,res) => {
-    res.send('response for /test')
-})
+// app.use('/test',(req,res) => {
+//     res.send('response for /test')
+// })
 
 
 
-app.post("/user/:userId",(req,res) => {
-    console.log(req.params)
-    res.send({firstName:"Zaheen",lastName:"Akhtar"})
-})
+// app.post("/user/:userId",(req,res) => {
+//     console.log(req.params)
+//     res.send({firstName:"Zaheen",lastName:"Akhtar"})
+// })
 
-app.get('/user',(req,res) => {
-    res.send('fetched user details')
-});
+// app.get('/user',(req,res) => {
+//     res.send('fetched user details')
+// });
 
-app.delete('/user/:userId',(req,res) => {
-    console.log(req.params)
-    res.send('delete user')
-})
+// app.delete('/user/:userId',(req,res) => {
+//     console.log(req.params)
+//     res.send('delete user')
+// })
 
-app.use("/",(req,res) => {
-    res.send('response for /')
+// app.use("/",(req,res) => {
+//     res.send('response for /')
+// })
+
+// app.use('/user',(req,res) => {
+//     console.log('first handler');
+//     res.send('first');
+// },(req,res) => {
+//     console.log('second handler');
+//     res.send('second');
+// })
+
+app.use('/user',(req,res,next) => {
+    console.log('1st route handler');
+    next();
+},(req,res,next) => {
+    console.log('2nd route handler');
+    next();
+},(req,res,next) => {
+    console.log('3rd route handler');
+    next();
+},(req,res,next) => {
+    console.log('4th route handler');
+    res.send('4th route handler');
 })
 
 app.listen(4000);
