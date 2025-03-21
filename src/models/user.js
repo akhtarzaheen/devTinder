9b346055c589c531d7d6db752caa0fa7285afb63
Zaheen335 <akhtarzaheen335@gmail.com>
2025-03-21 17:01:06 +0530

    Added validator

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose  = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
     firstName:{
@@ -13,10 +14,20 @@ const userSchema = new mongoose.Schema({
         type:String,
         required:true,
         unique:true,
-        trim:true
+        trim:true,
+        validate:(value) => {
+            if(!validator.isEmail(value)){
+                throw new Error("Email is not valid!");
+            }
+        }
     },
     password:{
-        type:String
+        type:String,
+        validate:(value) => {
+            if(!validator.isStrongPassword(value)){
+                throw new Error("Password is not strong!");
+            }
+        }
     },
     age:{
         type:Number,
