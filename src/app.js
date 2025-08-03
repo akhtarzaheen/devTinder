30af2831d9988e9dc561a3d88bdd7b1095c9be67
Zaheen335 <akhtarzaheen335@gmail.com>
2025-08-03 17:25:34 +0530

    added user auth middleware for the apis

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 require("./config/db");
 
+const {authUser} = require("./middleware/auth");
+
 const {User}  = require("./models/user");
 
 const app = express();
@@ -58,101 +60,14 @@ app.post("/login",async (req,res) => {
 });
 
 // API to fetch profile details
-app.get("/profile",async (req,res) => {
+app.get("/profile",authUser,async (req,res) => {
     try{
-        let token = req.cookies.token;
-        if(token){
-            let decoded = jwt.verify(token,"Zaheen@2021");
-            console.log('decoded=>',decoded)
-            const user = await User.findOne({_id:decoded.id});
-            if(user){
-                res.send(user);
-            }else{
-                throw new Error("User not found!");
-            }
-        }else{
-            throw new Error("Unauthorized");
-        }
+        res.send(req.user);
     }catch(e){
         res.status(400).send(e.message);
     }
 });
 
-// API /get user by email
-
-app.post("/user",async (req,res) => {
-    const email = req.body.emailId;
-    try{
-        const user = await User.find({emailId:email});
-        if(user){
-            res.send(user);
-        }else{
-            res.status(404).send("User not found!");
-        }
-    }catch(e){
-        res.status(400).send(e.message);
-    }
-});
-
-// Fetch all users from database
-app.get('/feed',async (req,res) => { 
-    try{
-        let users = await User.find({});
-        if(users){
-            res.send(users);
-        }else{
-            res.status(404).send('Users not found!')
-        }
-    }catch(e){
-        res.status(400).send(e.message);
-    }
-})
-
-// Delete user
-app.delete("/user",async (req,res) => {
-    let userId = req.body.userId;
-    try{
-        await User.findByIdAndDelete(userId);
-        res.send('User deleted successfully!');
-    }catch(e){
-        res.status(400).send(e.message);
-    }
-});
-
-// Update User database with id
-app.patch("/user",async (req,res) => {
-    let data = req.body;
-    let userId = req.body.userId;
-    let ALLOWED_DATA = [
-        "userId",
-        "firstName",
-        "lastName",
-        "emailId",
-        "password",
-        "age",
-        "gender",
-        "skills"
-    ]
-   
-    try{
-        let isDataAllowed = Object.keys(data).every((key) => {
-            if(ALLOWED_DATA.includes(key)){
-                return true
-            }
-            return false;
-        })
-        if(!isDataAllowed){
-            throw new Error('Data insertion not allowed');
-        }
-        if(req.body.skills.length>3){
-            throw new Error('skills can not be more than 3');
-        }
-        await User.findByIdAndUpdate(userId,data);
-        res.send('User updated successfully!');
-    }catch(e){
-        res.status(400).send(e.message);
-    }
-})
 
 dbConnect().then(()=> { 
     console.log('connection with clustered successfully!');
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,24 +1,26 @@
-const authAdmin = (req,res,next) => {
-    let token = "xyz";
-    if(token !== "xyz"){
-        res.status(401).send("Authorized Admin");
-    }else{
-        console.log('Authorized admin');
-        next();
-    }
-}
+const jwt = require("jsonwebtoken");
+const {User} = require("../models/user");
 
-const authUser = (req,res,next) => {
-    let token = "xyz";
-    if(token !== "xyz"){
-        res.status(401).send("Authorized Admin");
-    }else{
-        console.log('Authorized user');
-        next();
+const authUser = async (req,res,next) => {
+    try{    
+        let token = req.cookies.token;
+        if(!token){
+            throw new Error('Token not found');
+        }else{
+            let decoded = jwt.verify(token,"Zaheen@2021");
+            const {id} = decoded;
+            let user = await User.findById(id);
+            if(!user){
+                throw new Error('User not found');
+            }
+            req.user = user;
+            next();
+        }
+    }catch(error){
+        res.status(400).send("Unauthorized user!");
     }
 }
 
 module.exports = {
-    authAdmin,
     authUser
 }
