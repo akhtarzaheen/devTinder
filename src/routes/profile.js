311fa5425ff062dd53ce0befa16a6ab6b9c2cf07
Zaheen335 <akhtarzaheen335@gmail.com>
2025-08-05 00:06:10 +0530

    added forgot api route

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { authUser } = require("../middleware/auth");
 const { validateFields } = require("../utils/utils");
+const bcrypt = require("bcrypt");
 const profileRouter = express.Router();
 
 profileRouter.get("/profile/view",authUser,async (req,res) => {
@@ -27,6 +28,32 @@ profileRouter.patch('/profile/edit',authUser,async (req,res) => {
         }
     }catch(e){
         res.status(400).send(e.message);
+    }
+})
+
+profileRouter.patch("/profile/password",authUser,async(req,res) => {
+    try{
+        let user = req.user;
+        let {currentPassword,newPassword,confirmPassword} = req.body;
+        if(user){
+            let isPasswordValid = await user.verifyPassword(currentPassword);
+            if(isPasswordValid){
+               if(newPassword === confirmPassword){
+                let hashPassword = await bcrypt.hashSync(newPassword,10);
+                   user.password = hashPassword;
+                   await user.save();
+                   res.status(200).send('Password updated successfully!');
+               }else{
+                   throw new Error("Passwords do not match!");
+               } 
+            }else{
+                throw new Error("Current password is not valid!");
+            }
+        }else{
+            throw new Error("User not found!");
+        }
+    }catch(error){
+        res.status(400).send(error.message);
     }
 })
 
